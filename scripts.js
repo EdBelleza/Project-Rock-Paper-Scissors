1a400fa1d6f152dc8d084ae0920aa01fb9ecdd6a
Ed Belleza <119173327+stratusraiine@users.noreply.github.com>
2023-07-01 17:44:43 +0800

    Initial commit with partial finish

---
+++ scripts.js
@@ -0,0 +1,57 @@
+
+function getUserChoice() {
+    const userChoice = prompt('What hand would you like to play? [Rock, Paper, Scissors]').toLowerCase()
+    return userChoice
+}
+function getComputerChoice() {
+    const computerHands = ['rock', 'paper', 'scissors']
+    const computerChoice = computerHands[Math.floor(Math.random() * computerHands.length)]
+    return computerChoice
+}
+
+function playRound(playerSelection, computerSelection) {
+    let result;
+    if (playerSelection === computerSelection) {
+        result = `It's a tie!`
+    } else if ((playerSelection === 'rock' && computerSelection === 'scissors') || (playerSelection === 'paper' && computerSelection === 'rock') || (playerSelection === 'scissors' && computerSelection === 'paper')) {
+        result = `You win!`
+    } else {
+        result = `You lose`
+    }
+    console.log(result)
+    return result
+}
+let userScore = 0;
+let computerScore = 0;
+
+function game() {
+    const result = playRound(getUserChoice(), getComputerChoice());
+
+    if (result === `You win!`) {
+        userScore++;
+    } else if (result === `You lose`) {
+        computerScore++;
+    }
+
+    if (userScore === 5 || computerScore === 5) {
+        if (userScore > computerScore) {
+            return alert(`You won against the computer. Your score is ${userScore} while the computer has ${computerScore}`);
+        } else if (userScore < computerScore) {
+            return alert(`You lost against the computer. Your score is ${userScore} while the computer has ${computerScore}`);
+        }
+    }
+}
+game();
+game();
+game();
+game();
+
+game();
+game();
+game();
+game();
+game();
+game();
+game();
+
+
