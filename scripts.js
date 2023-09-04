9994d24c27935c5e18186dc814b24d33ad106482
Ed MACB <119173327+edmacb@users.noreply.github.com>
2023-09-04 20:34:38 +0800

    added ui

--- scripts.js
+++ scripts.js
@@ -1,57 +1,51 @@
+const rockChoice = document.getElementById("rock");
+const paperChoice = document.getElementById("paper");
+const scissorsChoice = document.getElementById("scissors");
 
-function getUserChoice() {
-    const userChoice = prompt('What hand would you like to play? [Rock, Paper, Scissors]').toLowerCase()
-    return userChoice
-}
 function getComputerChoice() {
-    const computerHands = ['rock', 'paper', 'scissors']
-    const computerChoice = computerHands[Math.floor(Math.random() * computerHands.length)]
-    return computerChoice
+  const computerHands = ["rock", "paper", "scissors"];
+  const computerChoice =
+    computerHands[Math.floor(Math.random() * computerHands.length)];
+  return computerChoice;
 }
 
 function playRound(playerSelection, computerSelection) {
-    let result;
-    if (playerSelection === computerSelection) {
-        result = `It's a tie!`
-    } else if ((playerSelection === 'rock' && computerSelection === 'scissors') || (playerSelection === 'paper' && computerSelection === 'rock') || (playerSelection === 'scissors' && computerSelection === 'paper')) {
-        result = `You win!`
-    } else {
-        result = `You lose`
-    }
-    console.log(result)
-    return result
+  let result;
+  if (playerSelection === computerSelection) {
+    result = `It's a tie!`;
+  } else if (
+    (playerSelection === "rock" && computerSelection === "scissors") ||
+    (playerSelection === "paper" && computerSelection === "rock") ||
+    (playerSelection === "scissors" && computerSelection === "paper")
+  ) {
+    result = `You win!`;
+  } else {
+    result = `You lose`;
+  }
+  console.log(result);
+  return result;
 }
 let userScore = 0;
 let computerScore = 0;
 
 function game() {
-    const result = playRound(getUserChoice(), getComputerChoice());
+  const result = playRound(getUserChoice(), getComputerChoice());
 
-    if (result === `You win!`) {
-        userScore++;
-    } else if (result === `You lose`) {
-        computerScore++;
-    }
+  if (result === `You win!`) {
+    userScore++;
+  } else if (result === `You lose`) {
+    computerScore++;
+  }
 
-    if (userScore === 5 || computerScore === 5) {
-        if (userScore > computerScore) {
-            return alert(`You won against the computer. Your score is ${userScore} while the computer has ${computerScore}`);
-        } else if (userScore < computerScore) {
-            return alert(`You lost against the computer. Your score is ${userScore} while the computer has ${computerScore}`);
-        }
+  if (userScore === 5 || computerScore === 5) {
+    if (userScore > computerScore) {
+      return alert(
+        `You won against the computer. Your score is ${userScore} while the computer has ${computerScore}`
+      );
+    } else if (userScore < computerScore) {
+      return alert(
+        `You lost against the computer. Your score is ${userScore} while the computer has ${computerScore}`
+      );
     }
+  }
 }
-game();
-game();
-game();
-game();
-
-game();
-game();
-game();
-game();
-game();
-game();
-game();
-
-
